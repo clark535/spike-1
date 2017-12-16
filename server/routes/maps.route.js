c4a776efbb3a2f3a6b83ca44aa0affdcd6bf36ba
David Clark <primecomp@Davids-MacBook-Pro.local>
2017-12-15 18:18:08 -0600

    Finished Spike

--- server/routes/maps.route.js
+++ server/routes/maps.route.js
@@ -24,8 +24,3 @@ router.get('/', function(req, res){
 });
 
 module.exports = router;
-
-// https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
-// &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
-// &markers=color:red%7Clabel:C%7C40.718217,-73.998284
-// &key=AIzaSyBXCCVmaJXKRVD4t_t3cCr-pDpV1gy16Gs
